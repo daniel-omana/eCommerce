b2ada1b0bb8f20b4f0ed90ec60f5bb91af6d5ddb
Daniel <danielomana@shaw.ca>
2020-11-26 02:32:34 -0600

    Applied validations and set up controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :manufacturer
   belongs_to :category
+
+  validates :name, :description, :retail_price, :sale_price, :quantity, presence: true
+  validates :name, uniqueness: true
+  validates :quantity, numericality: { only_integer: true }
+  validates :retail_price, :sale_price, numericality: true
 end
---
+++ db/schema.rb
@@ -0,0 +1,43 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_11_26_082414) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "manufacturers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "retail_price"
+    t.decimal "sale_price"
+    t.integer "quantity"
+    t.integer "manufacturer_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["manufacturer_id"], name: "index_products_on_manufacturer_id"
+  end
+
+  add_foreign_key "products", "categories"
+  add_foreign_key "products", "manufacturers"
+end
