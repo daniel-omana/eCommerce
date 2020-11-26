767ae76a52e18e78adfe6aa0f0394603d56c6f9d
Daniel <danielomana@shaw.ca>
2020-11-26 03:01:59 -0600

    added active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :categories, only: %i[index show]
   resources :products, only: %i[index show]
   resources :manufacturers, only: %i[index show]
