0f300559bc1afa38b32a5c819db6e89c00a98994
Daniel <danielomana@shaw.ca>
2020-11-26 02:26:19 -0600

    Created categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/show'
   get 'manufacturers/index'
   get 'manufacturers/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
