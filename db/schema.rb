ad0ff37ab96283368e56e3a704c0e8c181a62a6d
Daniel <danielomana@shaw.ca>
2020-11-27 00:30:19 -0600

    Created Order Item Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_062657) do
+ActiveRecord::Schema.define(version: 2020_11_27_063004) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -71,6 +71,17 @@ ActiveRecord::Schema.define(version: 2020_11_27_062657) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.decimal "price"
+    t.integer "product_id", null: false
+    t.integer "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.boolean "fulfilled"
     t.decimal "total"
@@ -103,6 +114,8 @@ ActiveRecord::Schema.define(version: 2020_11_27_062657) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "manufacturers"
 end
